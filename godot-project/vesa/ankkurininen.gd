ecdb6551bdee628143dded54a85df7f6759898b4
VesaVesa <87971172+VesaVesa@users.noreply.github.com>
2025-01-25 21:31:18 +0200

    ADDXC
    s<zfc

--- godot-project/vesa/ankkurininen.gd
+++ godot-project/vesa/ankkurininen.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 func _physics_process(delta: float) -> void:
 	pass
+<<<<<<< Updated upstream
 
 func _on_body_entered(body: Node2D) -> void:
 	(body as Cluster).gravity_scale = 1;
+=======
+>>>>>>> Stashed changes
