7f68d726cc20ea78f49f804b9aa515798990a1cd
tokko <tomi@valohai.com>
2025-01-25 22:26:45 +0200

    esc to close

--- godot-project/paavalikko.gd
+++ godot-project/paavalikko.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_pressed("ui_cancel"):
+
+func _input(event):
+	if event.is_action_released("ui_cancel"):
 		get_tree().quit()
--- godot-project/riku/maailma.gd
+++ godot-project/riku/maailma.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_pressed("ui_cancel"):
+func _input(event):
+	if event.is_action_released("ui_cancel"):
 		get_tree().change_scene_to_file("res://paavalikko.tscn")
