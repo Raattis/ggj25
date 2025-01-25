bc35161b73e9fe6feba1814e0d35590ce019ef58
Janne Hiiskoski <janne@hiiskoski.fi>
2025-01-25 11:34:21 +0200

    rendauskoordinaatisto toimii

--- godot-project/riku/kupla_coll.gd
+++ godot-project/riku/kupla_coll.gd
@@ -1,6 +1,9 @@
 extends CollisionShape2D
 
 func _process(delta: float) -> void:
-	var asdf: SceneVis = scenevis.find_child("ColorRect")
-	# TODO nää kuntoon
-	asdf.push_bubble((global_position - Vector2(500, 200)) / 1000, 0.05)
+	var renderer: SceneVis = scenevis.find_child("BubbleSceneRenderer")
+	var view_size = get_viewport_rect().size
+	renderer.push_bubble(
+		(global_position - view_size / 2.0) / view_size.y,
+		shape.get_radius() / view_size.y
+	)
