acdc9b2361df24498c2be690126f2b2d78881eae
Janne Hiiskoski <janne@hiiskoski.fi>
2025-01-25 11:52:30 +0200

    fix poks anim frame indexes

--- godot-project/vesa/poks.gd
+++ godot-project/vesa/poks.gd
@@ -1,13 +1,14 @@
 extends Sprite2D
 var passed_time = 0
 var duration = 0.05;
-var frame_duration = duration / 4;
+var frame_duration = duration / 4.0;
 func _ready():
 	pass
 
 func _process(delta: float):
-	passed_time += delta;
-	frame = passed_time / frame_duration;
-	if passed_time > duration:
+	var frame_index = passed_time / frame_duration;
+	if frame_index >= 4:
 		queue_free();;;;;
-	pass
+		return
+	passed_time += delta;
+	frame = frame_index
