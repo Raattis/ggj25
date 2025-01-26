8e96a9e9a7dc347a345fa7606fa76b07da403339
Raattis <raattis@gmail.com>
2025-01-26 14:15:02 +0200

    Fix kupla_nam kun the kalanen puuttuu

--- godot-project/vesa/kupla_nam.gd
+++ godot-project/vesa/kupla_nam.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
 
-func _on_body_entered(body: Node2D) -> void:
-	get_parent().find_child("the kalanen").etippä_toi(body);
-	pass # Replace with function body.
+func _on_body_entered(body) -> void:
+	if get_parent().find_child("the kalanen") and body is Cluster:
+		get_parent().find_child("the kalanen").etippä_toi(body);
