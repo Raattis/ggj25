3a44d75cd11a21da8babd1538ab28fa233edaae3
Janne Hiiskoski <janne@hiiskoski.fi>
2025-01-26 13:37:43 +0200

    tausta kamera liikuhan

--- godot-project/janne/bubblescene.gd
+++ godot-project/janne/bubblescene.gd
@@ -15,22 +15,26 @@ func push_bubble(pos: Vector2, radius: float, extra: float = 0.0):
 
 func _process(_delta: float) -> void:
 	var mat: ShaderMaterial = material;
-	
+
 	if !orig_camera_initialized && get_viewport().get_camera_2d():
 		orig_camera_pos = get_viewport().get_camera_2d().position
 		orig_camera_initialized = true
-		
+
 	if orig_camera_initialized:
 		var camera = get_viewport().get_camera_2d()
 		if not camera:
 			return
 		get_parent().position = camera.position - get_parent().size / 2.
-		
+
 		var camera_offset := get_viewport().get_camera_2d().position - orig_camera_pos
 		# HEI JANNE! Tassa sulle kameran offsetti :)
 		var normalized = camera_offset / size
 		mat.set_shader_parameter("camera_offset",normalized )
-	
+
+		# Päivittele taustan cameraaaaaaaaa
+		var bgscene_camera: Camera3D = get_parent().find_child("Camera3D")
+		bgscene_camera.position.y = 1.5 - normalized.y * 8.;
+
 	var aspect_ratio = size.x / size.y
 	mat.set_shader_parameter("aspect_ratio", aspect_ratio)
 	
@@ -38,7 +42,7 @@ func _process(_delta: float) -> void:
 	mat.set_shader_parameter("bubbles_pos", bubbles_pos)
 	mat.set_shader_parameter("bubbles_radius", bubbles_radius)
 	mat.set_shader_parameter("bubbles_extra", bubbles_extra)
-	
+
 	bubbles_pos.clear()
 	bubbles_radius.clear()
 	bubbles_extra.clear()
