e08fd59e8034e2f9bf26cfc84d5de3ac5ba18274
Janne Hiiskoski <janne@hiiskoski.fi>
2025-01-26 12:59:03 +0200

    Enterprise refactoring

--- godot-project/janne/bubblescene.gd
+++ godot-project/janne/bubblescene.gd
@@ -14,11 +14,20 @@ func push_bubble(pos: Vector2, radius: float, extra: float = 0.0):
 	bubbles_extra.push_back(extra)
 
 func _process(_delta: float) -> void:
+	var mat: ShaderMaterial = material;
+	
 	if !orig_camera_initialized && get_viewport().get_camera_2d():
 		orig_camera_pos = get_viewport().get_camera_2d().position
 		orig_camera_initialized = true
-	
-	var mat: ShaderMaterial = material;
+		
+	if orig_camera_initialized:
+		var camera = get_viewport().get_camera_2d()
+		get_parent().position = camera.position - get_parent().size / 2.
+		
+		var camera_offset := get_viewport().get_camera_2d().position - orig_camera_pos
+		# HEI JANNE! Tassa sulle kameran offsetti :)
+		var normalized = camera_offset / size
+		mat.set_shader_parameter("camera_offset",normalized )
 	
 	var aspect_ratio = size.x / size.y
 	mat.set_shader_parameter("aspect_ratio", aspect_ratio)
@@ -31,12 +40,3 @@ func _process(_delta: float) -> void:
 	bubbles_pos.clear()
 	bubbles_radius.clear()
 	bubbles_extra.clear()
-	
-	if orig_camera_initialized:
-		var camera = get_viewport().get_camera_2d()
-		get_parent().position = camera.position - get_parent().size / 2.
-		
-		var camera_offset := get_viewport().get_camera_2d().position - orig_camera_pos
-		# HEI JANNE! Tassa sulle kameran offsetti :)
-		var normalized = camera_offset / size
-		mat.set_shader_parameter("camera_offset",normalized )
