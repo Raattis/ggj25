77e0f3c056ba0609af68b3f36703811ce3c9460a
Raattis <raattis@gmail.com>
2025-01-26 14:09:42 +0200

    piikit pitaa skaalansa

--- godot-project/riku/piikki_chilling.gd
+++ godot-project/riku/piikki_chilling.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
 
+
+@onready var coll = $"../coll"
+
 var o := 0.0
 
 func _ready():
@@ -9,3 +12,4 @@ func _ready():
 func _process(delta):
 	position.y = sin(Time.get_ticks_msec() / 300.0 + o)
 	position.x = sin(Time.get_ticks_msec() / 250.0 + 50 + o)
+	scale = Vector2(1,1) * coll.shape.radius / 5.0 * 0.03
