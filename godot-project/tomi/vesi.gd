9cf7d495864114aac1c578278437d4d6e9887f8e
tokko <tomi@valohai.com>
2025-01-26 15:15:58 +0200

    nicer game

--- godot-project/tomi/vesi.gd
+++ godot-project/tomi/vesi.gd
@@ -1,6 +1,14 @@
 extends Sprite2D
 
+var t = 0.0
 
 func _input(event):
 	if event.is_pressed() and event.as_text() == "T":
 		visible = not visible
+		
+func _process(delta):
+	t += delta
+	position = Vector2(
+		1000 + 50 * sin(t * .05),
+		1000 + 50 * sin((t + 1) * .051),
+	)
